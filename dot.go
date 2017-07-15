7721a2b63f117129df348db2cf45fa04ca14201f
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-15 16:23:28 +0200

    new: dot.go
    just a noop - avoids 'no buildable Go source files'-warning of `go get`

---
+++ dot.go
@@ -0,0 +1,8 @@
+﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// directory container provides two subdirectories with 'normal' and concurrncy-safe 'ccsafe' containers
+package container
+
+const dot = `.` // just a noop - avoids 'no buildable Go source files' warning of `go get`
